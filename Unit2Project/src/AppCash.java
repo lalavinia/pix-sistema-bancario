905b16b74130e2c6802e27a31505f561489ac3e8
lalavinia <78618322+lalavinia@users.noreply.github.com>
2021-02-08 02:33:34 -0300

    Delete Unit2Project directory

--- Unit2Project/src/AppCash.java
+++
@@ -1,6 +0,0 @@
-
-public class AppCash {
-	public static void main(String[] args) {
-		
-	}
-}
--- Unit2Project/src/CentralBank.java
+++
@@ -1,9 +0,0 @@
-
-public class CentralBank {
-	// atributos
-	private Bank[] banks;
-	private Pix[] idPixs;
-	// métodos
-	
-	
-}
